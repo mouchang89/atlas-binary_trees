18148a3049f070699261e841688426f683542b81
mouchang89 <mou.chang@atlasschool.com>
2024-04-18 18:55:24 +0000

    removed NULL statement from balance function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,14 +8,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else if (tree)
+	if (tree)
 	{
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	}
+	return (0);
 }
 
 /**
