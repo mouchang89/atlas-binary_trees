f159e165ab3f5d7848491389c1b6a1f00479bca6
mouchang89 <mou.chang@atlasschool.com>
2024-04-18 17:30:59 +0000

    fixed trailing white spaces

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 	if (tree->left == NULL && tree->right == NULL)
-	{	
+	{
 		return (0);
 	}
 	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
@@ -25,4 +25,3 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (binary_tree_height(tree->right) + 1);
 	}
 }
-
