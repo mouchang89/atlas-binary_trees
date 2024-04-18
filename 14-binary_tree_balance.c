7496c11b2d0c0026cb54def47bfb2ca261c41f39
mouchang89 <mou.chang@atlasschool.com>
2024-04-18 18:52:20 +0000

    copy and pasted height function from github

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,12 +34,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	else if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
 	{
 		return (binary_tree_height(tree->left) + 1);
 	}
 	else
 	{
-		return (binary_tree_height(tree->right) + 1;
+		return (binary_tree_height(tree->right) + 1);
 	}
 }
