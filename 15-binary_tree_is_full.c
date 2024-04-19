140e2b4c691afd56499b3270fc5f8d9ab0a7b788
mouchang89 <mou.chang@atlasschool.com>
2024-04-19 01:14:28 +0000

    added && to else if statement return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,7 +18,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	else if (tree->left && tree->right)
 	{
-		return (binary_tree_is_full(tree->left) binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
 	return (0);
 }
